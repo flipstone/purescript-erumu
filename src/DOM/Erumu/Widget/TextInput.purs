90071b4e3d1bf845c1423532555eb358c430560f
Rob Magary <rob.flipstone.com>
2026-06-22 13:08:54 -0400

    [sc-50634] Reverts TextInput helpers and InputType in favor of just passing custom properties in the user props on the renderWith fuction

--- src/DOM/Erumu/Widget/TextInput.purs
+++ src/DOM/Erumu/Widget/TextInput.purs
@@ -1,23 +1,20 @@
 module DOM.Erumu.Widget.TextInput
-  ( Model
-  , Msg
-  , disabled
+  ( Msg
+  , Model
   , empty
+  , withValue
+  , setValue
+  , setDisabled
+  , disabled
+  , value
   , isBlank
   , render
   , renderWith
-  , setDisabled
-  , setEmailInputType
-  , setPasswordInputType
-  , setValue
   , update
-  , value
-  , withValue
   ) where
 
 import Prelude
 
-import Data.Newtype (class Newtype, unwrap)
 import DOM.Erumu.Form (disabledProp)
 import DOM.Erumu.HTML (input, type_)
 import DOM.Erumu.HTML as HTML
@@ -29,28 +26,18 @@ newtype Model = Model Fields
 type Fields =
   { currentValue :: String
   , disabled :: Boolean
-  , inputType :: InputType
   }
 
 newtype Msg = NewInput String
 
-newtype InputType = InputType String
-
-derive instance Newtype InputType _
-
--- | The value of the input together with its type, which determines how
--- | the value is rendered and updated. The builder defaults to text,
--- | but also supports password and email inputs using the `setPasswordInputType` and `setEmailInputType` functions.
 empty :: Model
 empty = withValue ""
 
--- | Create a new `Model` with the given value, preserving the default input type (text) and disabled state (false).
 withValue :: String -> Model
 withValue s =
   Model
     { currentValue: s
     , disabled: false
-    , inputType: InputType "text"
     }
 
 value :: Model -> String
@@ -67,16 +54,6 @@ setDisabled :: Boolean -> Model -> Model
 setDisabled bool (Model m) =
   Model m { disabled = bool }
 
--- | Set the input type to "password", which renders the value as obscured dots and prevents autofill.
-setPasswordInputType :: Model -> Model
-setPasswordInputType (Model m) =
-  Model m { inputType = InputType "password" }
-
--- | Set the input type to "email", which provides email-specific input behavior and validation.
-setEmailInputType :: Model -> Model
-setEmailInputType (Model m) =
-  Model m { inputType = InputType "email" }
-
 disabled :: Model -> Boolean
 disabled (Model m) = m.disabled
 
@@ -92,7 +69,7 @@ renderWith ::
 renderWith liftMsg userProps (Model m) =
   let
     ourProps =
-      [ type_ (unwrap m.inputType)
+      [ type_ "text"
       , onEventDecode "oninput" (liftMsg <<< NewInput <$> inputValue)
       , HTML.value m.currentValue
       ]
